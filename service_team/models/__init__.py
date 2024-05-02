69b40d50f4f7693a97e0dba74a081369503b4733
SejPatel <38757553+SejPatel@users.noreply.github.com>
2024-05-02 19:08:23 +0530

    Delete service_team/models directory

--- service_team/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import masters
-from . import team
-from . import account
-from . import scheduling
-from . import partner
-from . import calendar_job
-from . import purchase
